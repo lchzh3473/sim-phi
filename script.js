ca43b7a62505d5275e4d1b97894f878ba0d6f2b2
lchzh3473 <lchzh3473@163.com>
2021-04-19 00:40:57 +0800

    commit

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 "use strict";
 const _i = ['Phigros模拟器', [1, 0, 1], 1618728604, 1611795955];
 const canvas = document.getElementById("stage");
+const canvasbg = document.getElementById("bg");
 //调节尺寸
-canvas.width = window.innerWidth * window.devicePixelRatio;
-canvas.height = window.innerHeight * window.devicePixelRatio;
-let maxlen = Math.sqrt(canvas.width ** 2 + canvas.height ** 2);
-let wlen = canvas.width / 2;
-let hlen = canvas.height / 2;
-let mlen = Math.min(wlen, hlen);
-let scale = canvas.width / 6e3; //note缩放
-window.addEventListener("resize", () => {
-	canvas.width = window.innerWidth * window.devicePixelRatio;
+let maxlen, wlen, hlen, mlen, scale, sx, sy, sw, sh, dx1, dy1, dw1, dh1, dx2, dy2, dw2, dh2; //背景图相关
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
+
+function resizeCanvas() {
+	canvas.width = Math.min(window.innerWidth, window.innerHeight * 16 / 9) * window.devicePixelRatio;
 	canvas.height = window.innerHeight * window.devicePixelRatio;
 	maxlen = Math.sqrt(canvas.width ** 2 + canvas.height ** 2);
 	wlen = canvas.width / 2;
 	hlen = canvas.height / 2;
 	mlen = Math.min(wlen, hlen);
-	scale = canvas.width / 6e3;
-});
+	scale = canvas.width / 6e3; //note缩放
+	canvasbg.width = window.innerWidth * window.devicePixelRatio;
+	canvasbg.height = window.innerHeight * window.devicePixelRatio;
+	if (canvasbg.width * 9 > canvasbg.height * 16) {
+		dx1 = (canvasbg.width - canvasbg.height * 16 / 9) / 2;
+		dy1 = 0;
+		dw1 = canvasbg.height * 16 / 9;
+		dh1 = canvasbg.height;
+		dx2 = 0;
+		dy2 = (canvasbg.height - canvasbg.width * 9 / 16) / 2;
+		dw2 = canvasbg.width;
+		dh2 = canvasbg.width * 9 / 16;
+	} else {
+		dx1 = (canvasbg.width - canvasbg.height * 16 / 9) / 2;
+		dy1 = 0;
+		dw1 = canvasbg.height * 16 / 9;
+		dh1 = canvasbg.height;
+	}
+}
 //谱面默认信息
-let frame = 0;
-let bpm = 0;
-let duration = 120;
+let bpm, duration, frame = 0;
 //bpm事件
 const bpmEvents = []; //存放bpm事件
 class bpmEvent {
@@ -59,49 +72,60 @@ class clickEvent {
 	}
 }
 //适配PC鼠标
-let isMouseDown = false;
+let isMouseDown = [];
 canvas.addEventListener("mousedown", evt => {
 	evt.preventDefault();
-	if (isMouseDown) mouseup();
-	else {
-		clicks[0] = {
-			x: evt.pageX * window.devicePixelRatio,
-			y: evt.pageY * window.devicePixelRatio,
-		};
-		clickEvents.push(new clickEvent(clicks[0].x, clicks[0].y));
-		clicks[0].t = setInterval(() => clickEvents.push(new clickEvent(clicks[0].x, clicks[0].y)), 200);
-		isMouseDown = true;
-	}
+	const idx = evt.button;
+	console.log("mousedown", idx); //test
+	clicks[idx] = {
+		x: (evt.pageX - canvas.offsetLeft) * window.devicePixelRatio + canvas.width / 2,
+		y: evt.pageY * window.devicePixelRatio,
+	};
+	clickEvents.push(new clickEvent(clicks[idx].x, clicks[idx].y));
+	clicks[idx].t = setInterval(() => clickEvents.push(new clickEvent(clicks[idx].x, clicks[idx].y)), 200);
+	isMouseDown[idx] = true;
 });
 canvas.addEventListener("mousemove", evt => {
 	evt.preventDefault();
-	if (isMouseDown) {
-		clicks[0].x = evt.pageX * window.devicePixelRatio;
-		clicks[0].y = evt.pageY * window.devicePixelRatio;
+	for (const idx in isMouseDown) {
+		if (isMouseDown[idx]) {
+			//console.log("mousemove", idx); //test
+			clicks[idx].x = (evt.pageX - canvas.offsetLeft) * window.devicePixelRatio + canvas.width / 2;
+			clicks[idx].y = evt.pageY * window.devicePixelRatio;
+		}
 	}
 });
 canvas.addEventListener("mouseup", evt => {
 	evt.preventDefault();
-	if (isMouseDown) mouseup();
+	const idx = evt.button;
+	console.log("mouseup", idx); //test
+	clearInterval(clicks[idx].t);
+	clicks[idx] = {};
+	isMouseDown[idx] = false;
 });
-
-function mouseup() {
-	clearInterval(clicks[0].t);
-	clicks[0] = {};
-	isMouseDown = false;
-}
+canvas.addEventListener("mouseout", evt => {
+	evt.preventDefault();
+	for (const idx in isMouseDown) {
+		if (isMouseDown[idx]) {
+			console.log("mouseout", idx); //test
+			clearInterval(clicks[idx].t);
+			clicks[idx] = {};
+			isMouseDown[idx] = false;
+		}
+	}
+});
 //适配移动设备
 const passive = {
 	passive: false
-};
+}; //不加这玩意会出现warning
 canvas.addEventListener("touchstart", evt => {
-	evt.preventDefault();
 	for (const i of evt.changedTouches) {
 		const idx = i.identifier;
+		console.log("touchstart", idx); //test
 		clicks[idx] = {
-			x: i.pageX * window.devicePixelRatio,
+			x: (i.pageX - canvas.offsetLeft) * window.devicePixelRatio + canvas.width / 2,
 			y: i.pageY * window.devicePixelRatio
-		};
+		}; //移动端存在多押bug(可能已经解决了？)
 		clickEvents.push(new clickEvent(clicks[idx].x, clicks[idx].y));
 		clicks[idx].t = setInterval(() => clickEvents.push(new clickEvent(clicks[idx].x, clicks[idx].y)), 200);
 	}
@@ -110,23 +134,28 @@ canvas.addEventListener("touchmove", evt => {
 	evt.preventDefault();
 	for (const i of evt.changedTouches) {
 		const idx = i.identifier;
-		if (idx >= 0) {
-			clicks[idx].x = i.pageX * window.devicePixelRatio;
-			clicks[idx].y = i.pageY * window.devicePixelRatio;
-		}
+		//console.log("touchmove",idx); //test
+		clicks[idx].x = (i.pageX - canvas.offsetLeft) * window.devicePixelRatio + canvas.width / 2;
+		clicks[idx].y = i.pageY * window.devicePixelRatio;
 	}
 }, passive);
 canvas.addEventListener("touchend", evt => {
 	evt.preventDefault();
 	for (const i of evt.changedTouches) {
 		const idx = i.identifier;
-		if (idx >= 0) {
-			clearInterval(clicks[idx].t);
-			clicks[idx] = {};
-		}
+		console.log("touchend", idx); //test
+		clearInterval(clicks[idx].t);
+		clicks[idx] = {};
 	}
 });
-//
+canvas.addEventListener("touchcancel", evt => {
+	for (const i of evt.changedTouches) {
+		const idx = i.identifier;
+		console.log("touchcancel", idx); //test
+		clearInterval(clicks[idx].t);
+		clicks[idx] = {};
+	}
+});
 //判定线定义
 const lines = []; //存放判定线
 const lineEvents = []; //存放判定线事件
@@ -178,12 +207,20 @@ class note {
 	}
 }*/
 const ctx = canvas.getContext("2d");
+const ctxbg = canvasbg.getContext("2d");
 let time = Date.now();
 
 function draw() {
 	//重置画面
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	//绘制背景
+	ctxbg.drawImage(img.bg, sx, sy, sw, sh, dx2, dy2, dw2, dh2);
+	ctxbg.drawImage(img.bg, sx, sy, sw, sh, dx1, dy1, dw1, dh1);
+	//背景变暗
 	ctx.fillStyle = "#000";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.globalAlpha = 0.5;
+	ctx.fillRect(0, 0, dw1, dh1);
+	ctx.globalAlpha = 1;
 	//多押阴影
 	ctx.shadowColor = "#ff7";
 	//显示文字（时刻）
@@ -287,22 +324,67 @@ function draw() {
 	}
 	requestAnimationFrame(draw);
 }
-
 const actx = new AudioContext();
 const img = {};
 const imgClick = [];
 let curTime = 0;
 let curTimestamp = 0;
+const aud = [];
+let json = {};
+//test start
+let aaa = 0;
 
+function test() {
+	for (const i of json.judgeLineList[0].notesAbove) {
+		//console.log(i.time);
+		setTimeout(() => {
+			const bufferSource = actx.createBufferSource();
+			bufferSource.buffer = aud[type2idx(i.type)];
+			//bufferSource.loop = true; //循环播放
+			bufferSource.connect(actx.destination);
+			bufferSource.start();
+			console.log(i.type, ++aaa, (Array(7).join(0) + (1000000 / 2500 * aaa).toFixed(0)).slice(-7));
+		}, i.time * 6e4 / 200 / 32);
+	}
+
+	function type2idx(type) {
+		switch (type) {
+			case 1:
+			case 3:
+				return 0;
+			case 2:
+				return 1;
+			case 4:
+				return 2;
+			default:
+				return 0;
+		}
+	}
+}
+//test end
 function init() {
 	//谱面(临时，以后改成json)
-	bpmEvents.push(new bpmEvent(0, 170), new bpmEvent(127.14, 138));
+	//bpmEvents.push(new bpmEvent(0, 170), new bpmEvent(127.14, 138));
+	bpmEvents.push(new bpmEvent(0, 200));
 	lines.push(new line(0, 0, 0, 0, 1));
-	loadImage();
+	loadChart();
+	//加载谱面
+	function loadChart() {
+		let xhr = new XMLHttpRequest();
+		xhr.open("get", "test.json");
+		xhr.send();
+		xhr.onload = () => {
+			json = JSON.parse(xhr.responseText);
+			loadImage();
+			console.log(json);
+			console.log(json.judgeLineList[0].notesAbove); //test
+		}
+	}
 	//加载图片
 	function loadImage() {
 		const imgsrc = {
-			click: "1.png"
+			click: "1.png",
+			bg: "bg.png"
 		};
 		let imgNum = 0;
 		for ({} in imgsrc) imgNum++;
@@ -319,6 +401,17 @@ function init() {
 	}
 	//加载点击动画
 	function loadImage2() {
+		if (img.bg.width * 9 > img.bg.height * 16) {
+			sx = (img.bg.width - img.bg.height * 16 / 9) / 2;
+			sy = 0;
+			sw = img.bg.height * 16 / 9;
+			sh = img.bg.height;
+		} else {
+			sx = 0;
+			sy = (img.bg.height - img.bg.width * 9 / 16) / 2;
+			sw = img.bg.width;
+			sh = img.bg.width * 9 / 16;
+		}
 		const canvas2 = document.createElement("canvas");
 		canvas2.width = 512;
 		canvas2.height = 7680;
@@ -337,16 +430,35 @@ function init() {
 		}
 		loadAudio();
 	}
-	//初始化bgm
+
 	function loadAudio() {
+		const audsrc = ["./HitSong_0.ogg", "./HitSong_1.ogg", "./HitSong_2.ogg"];
+		let audNum = 0;
+		for ({} in audsrc) audNum++;
+		for (const i in audsrc) {
+			let xhr = new XMLHttpRequest(); //通过XHR下载音频文件
+			xhr.open("get", audsrc[i], true);
+			xhr.responseType = 'arraybuffer';
+			xhr.onload = () => { //下载完成
+				actx.decodeAudioData(xhr.response, data => aud[i] = data);
+				if (--audNum <= 0) {
+					loadAudio2();
+				}
+			};
+			xhr.send();
+		}
+	}
+	//初始化bgm
+	function loadAudio2() {
 		let xhr = new XMLHttpRequest(); //通过XHR下载音频文件
-		xhr.open("get", 'bgm.ogg', true);
+		xhr.open("get", 'test.ogg', true);
 		xhr.responseType = 'arraybuffer';
 		xhr.onload = () => { //下载完成
 			actx.decodeAudioData(xhr.response, data => {
 				duration = data.duration;
 				playBgm(data);
 				draw();
+				test(); //test
 			});
 		};
 		xhr.send();
@@ -373,7 +485,6 @@ function init() {
 	}
 }
 init();
-
 
 function drawTapNote(crd1, crd2) {
 	ctx.translate(wlen + crd1.wx * wlen + crd1.hx * hlen + crd1.mx * mlen, hlen - crd1.wy * wlen - crd1.hy * hlen - crd1.my * mlen);
