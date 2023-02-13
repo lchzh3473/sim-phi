e9a3102355e3a2298a97e84ef5a9c94d3e6f9f2e
lchzh3473 <lchzh3473@163.com>
2023-02-13 11:41:52 +0800

    fix bug

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import { audio } from '/utils/aup.js';
 import { full, Timer, getConstructorName, urls, isUndefined, loadJS, frameTimer, time2Str, orientation } from './js/common.js';
 import { uploader, readZip } from './js/reader.js';
 import InterAct from '/utils/interact.js';
-self._i = ['Phi\x67ros模拟器', [1, 4, 22, 'b19'], 1611795955, 1676065061];
+self._i = ['Phi\x67ros模拟器', [1, 4, 22, 'b20'], 1611795955, 1676259656];
 const $ = query => document.getElementById(query);
 const $$ = query => document.body.querySelector(query);
 const $$$ = query => document.body.querySelectorAll(query);
@@ -1125,7 +1125,7 @@ function playBgm(data, offset) {
 	isPaused = false;
 	if (!offset) offset = 0;
 	curTimestamp = performance.now();
-	audio.play(data, { offset: offset, playbackrate: app.speed, gainrate: app.musicVolume });
+	audio.play(data, { offset: offset, playbackrate: app.speed, gainrate: app.musicVolume, interval: 1 });
 }
 /** @param {HTMLVideoElement} data */
 function playVideo(data, offset) {
